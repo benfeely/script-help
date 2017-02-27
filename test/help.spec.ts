af657a8f5eb213b6bd9d52565c60de700474d794
Ben Feely <benfeely@microsoft.com>
2017-02-26 20:28:20 -0800

    Help test setup with stdout mock.

---
+++ test/help.spec.ts
@@ -0,0 +1,22 @@
+import { Package } from "../src/package";
+import { Help } from "../src/help";
+import { MockOutputStream } from "./outputStream.mock";
+
+describe("Help", () => {
+    const pkg = new Package(require("./config/test1/package.json"));
+    const outputStream = new MockOutputStream(process.stdout);
+    const help = new Help(pkg, <any>outputStream);
+
+    beforeEach(() => {
+        outputStream.reset();
+    });
+
+    it(`should not err`, () => {
+        should.not.Throw(() => help.write());
+    });
+
+    it(`should log to console`, () => {
+        help.write();
+        outputStream.messages.should.have.lengthOf(2);
+    });
+});
